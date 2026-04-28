fbfb6725d6a3b05ea7b321b1a6a1511c59a448b8
jupyter <irawanrafidzufar@gmail.com>
2026-04-28 14:43:37 +0700

    fix: Add favicon link to the main layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+        <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+
         <title>{{ $title ?? 'Bagi Kata' }}</title>
 
         <link rel="preconnect" href="https://fonts.googleapis.com">
